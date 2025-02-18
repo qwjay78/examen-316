c793e0007b95e3bf3b0c0e88b404597885635bbd
joana <joana.lps.pro@gmail.com>
2025-02-18 19:36:26 +0100

    add forum controller

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ForumTopicRepository;
+use App\Entity\ForumTopic;
+use App\Form\ForumTopicType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+
+
+
+class ForumController extends AbstractController{
+
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/forum', name: 'forum_index')]
+    public function index(ForumTopicRepository $topicRepository): Response
+    {
+        $topics = $topicRepository->findAll();
+        return $this->render('forum/index.html.twig', [
+            'topics' => $topics,
+        ]);
+    }
+
+#[Route('/forum/new', name: 'forum_new')]
+public function new(Request $request): Response
+{
+    $topic = new ForumTopic();
+    $form = $this->createForm(ForumTopicType::class, $topic);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $topic->setAuthor($this->getUser());
+        $topic->setCreatedAt(new \DateTimeImmutable());
+        $this->entityManager->persist($topic);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('forum_index');
+    }
+
+    return $this->render('forum/new.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+}
